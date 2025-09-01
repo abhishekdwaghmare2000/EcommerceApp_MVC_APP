ba60feb51a4665885af79325d0e8b4b23d0dfa52
Abhishek Waghmare <abhishekdwaghmare2000@gmail.com>
2025-09-01 18:56:27 +0530

    The changes till Shopping Cart implementation is Pushed

---
+++ Ecommerce.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get;  }
+        IProductRepository Product { get; }
+        ICompanyRepository Company { get; }
+        IShoppingCartRepository ShoppingCart { get; }
+
+        void Save();
+    }
+}
---
+++ Ecommerce.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using Ecommerce.DataAccess.Data;
+using Ecommerce.DataAccess.Repository.IRepository;
+using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+        public ICategoryRepository Category { get; private set; }
+        public ICompanyRepository Company { get; private set; }
+        public IProductRepository Product { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public UnitOfWork(ApplicationDbContext db) {
+            _db = db;
+            ShoppingCart = new ShoppingCartRepository(_db);
+            Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
+            Company = new CompanyRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
