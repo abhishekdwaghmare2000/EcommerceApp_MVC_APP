dee1be24a05d5c9d98b3e2cafdfb144534874dcc
Abhishek Waghmare <abhishekdwaghmare2000@gmail.com>
2025-09-06 12:02:10 +0530

    Shopping cart increment, decrement and remove items implemented with order total

--- Ecommerce.DataAccess/Repository/Repository.cs
+++ Ecommerce.DataAccess/Repository/Repository.cs
@@ -24,9 +24,17 @@ namespace Ecommerce.DataAccess.Repository
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null, bool tracked = false)
         {
-            IQueryable<T> query = dbSet;
+            IQueryable<T> query;
+            if (tracked)
+            {
+                query = dbSet;
+            }
+            else {
+                query = dbSet.AsNoTracking();
+            }
+
             query = query.Where(filter);
             if (!string.IsNullOrEmpty(includeProperties))
             {
@@ -38,9 +46,12 @@ namespace Ecommerce.DataAccess.Repository
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (filter != null) {
+                query = query.Where(filter);
+            }
             if (!string.IsNullOrEmpty(includeProperties)) { 
                foreach(var includeProp in includeProperties.Split(new char[] {',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
