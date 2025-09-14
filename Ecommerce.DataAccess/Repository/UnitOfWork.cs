72469289c88566094db894d802a4947d216f76eb
Abhishek Waghmare <abhishekdwaghmare2000@gmail.com>
2025-09-14 19:58:33 +0530

    Order- Confimartion - Implemented model and repository for OrderHeader and OrderDetail

--- Ecommerce.DataAccess/Repository/UnitOfWork.cs
+++ Ecommerce.DataAccess/Repository/UnitOfWork.cs
@@ -18,6 +18,9 @@ namespace Ecommerce.DataAccess.Repository
         public IShoppingCartRepository ShoppingCart { get; private set; }
 
         public IApplicationUserRepository ApplicationUser { get; private set; }
+
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
         public UnitOfWork(ApplicationDbContext db) {
             _db = db;
             ApplicationUser = new ApplicationUserRepository(_db);
@@ -25,6 +28,8 @@ namespace Ecommerce.DataAccess.Repository
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public void Save()
